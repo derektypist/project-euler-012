93f6dd8f8f2c3f362f335be1cd656975694b8959
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-04-20 10:53:31 +0000

    Added getNumberInfo function at script.js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Function to Get Number Information (including Invalid Input)
+function getNumberInfo() {
+    // Set Up Variable
+    let txt = "";
+    // Get the Value of the Input Field
+    let num = document.getElementById('mynumber').value;
+    // Check if the input is valid
+    if (isNaN(num) || num.toString().length == 0 || num<1 || num>1000 || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter a whole number between 1 and 1000.`;
+    } else {
+        txt += `You have entered the number ${num}. <p>`;
+        txt += `The first triangle number to have over ${num} divisors is ${divisibleTriangleNumber(num)}.`;
+    }
+
+    // Display Information in the Browser
+    document.getElementById("numinfo").innerHTML = txt;
+
+}
